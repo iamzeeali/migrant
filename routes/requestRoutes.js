fe7436db3232dbb46d513b035e2d28d650820e78
Zeeshan <mdzeeshanali93@gmail.com>
2020-06-01 13:02:33 +0530

    super admin permission added

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const requestController = require("./../controllers/requestController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
+
+//Protect all routes after this middleware
+router.use(authController.protect);
+
+router.use(authController.restrictTo("super-admin", "admin"));
 
 router
   .route("/")
--- routes/responseRoutes.js
+++ routes/responseRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //Protect all routes after this middleware
 router.use(authController.protect);
 
-router.use(authController.restrictTo("admin"));
+router.use(authController.restrictTo("super-admin", "admin"));
 router
   .route("/")
   .get(responseController.getAllResponses)
